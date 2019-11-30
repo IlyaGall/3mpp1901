4500f8bee0ad91e87c71bc6bbf4da9bea83b5d7f
Your Name <you@example.com>
2019-11-30 15:44:33 +0300

    First progect in Java

---
+++ hellowWord.java
@@ -0,0 +1,9 @@
+public class hellowWord
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello Java!");
+	}
+}
+// java E:\javaProcet\hellowWord.java
+// javac E:\javaProcet\hellowWord.java
