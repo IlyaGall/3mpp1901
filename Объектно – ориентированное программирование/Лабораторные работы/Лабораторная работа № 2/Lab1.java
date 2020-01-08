e56656cd0804ce430487bd3b91aa5fd36233eb9e
Ilya <ilyagall01@gmail.com>
2020-01-08 14:44:16 +0300

    Вторая лабораторная работа. Площадь 3-х мерного треугольника

--- Объектно – ориентированное программирование/Лабораторные работы/Лабораторная работа № 2/Lab1.java
+++ Объектно – ориентированное программирование/Лабораторные работы/Лабораторная работа № 2/Lab1.java
@@ -1,10 +1,38 @@
+import java.util.Scanner;
 public class Lab1
 {
     public static void main(String[] args)
     {
+        Scanner in = new Scanner(System.in);
+       /*
         Point3d point3dOne=new Point3d(1,1,0);  // создание 3-х точек в пространстве
         Point3d point3dTwo=new Point3d(-2 ,4,0);
         Point3d point3dThree=new Point3d(-2,-2,0);
+        */
+        Point3d point3dOne=new Point3d();  // создание 3-х точек в пространстве
+        Point3d point3dTwo=new Point3d();
+        Point3d point3dThree=new Point3d();
+        System.out.println("Fist point."+"\n" +" X=");
+        point3dOne.setX(in.nextDouble());
+        System.out.println(" y=");
+        point3dOne.setY(in.nextDouble());
+        System.out.println(" z=");
+        point3dOne.setZ(in.nextDouble());
+
+        System.out.println("Second point."+ "\n"  + "X=");
+        point3dTwo.setX(in.nextDouble());
+        System.out.println(" y=");
+        point3dTwo.setY(in.nextDouble());
+        System.out.println(" z=");
+        point3dTwo.setZ(in.nextDouble());
+
+        System.out.println("Three point." + "\n"  + "X=");
+        point3dThree.setX(in.nextDouble());
+        System.out.println("  y=");
+        point3dThree.setY(in.nextDouble());
+        System.out.println("  z=");
+        point3dThree.setZ(in.nextDouble());
+
 
 
         System.out.println( "S= "+   computeArea(point3dOne,point3dTwo,point3dThree));//передача данных для расчёта площади треугольника
@@ -29,6 +57,6 @@ public class Lab1
        double P=(a+b+c)/2;
        double s=Math.sqrt(P*(P-a)*(P-b)*(P-c));
        //альтернативный вариант выглядит так:  double s=Math.sqrt(P*(P-onePoint.distanceTo(twoPoint))*(P-twoPoint.distanceTo(threePoint))*(P-threePoint.distanceTo((onePoint))));
-       return  s;
+       return  Math.round(s * 100) / 100.0;
     }
 }
